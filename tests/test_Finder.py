78b4cee658894d95e66271d80b2612b05f428a55
leommxj <leommxj@gmail.com>
2023-05-29 00:00:36 +0800

    add some wired crc test

--- tests/test_Finder.py
+++ tests/test_Finder.py
@@ -9,3 +9,9 @@ def test_finder():
     r = f.findCrc(data)
     assert(len(r) == 1)
     assert(r[0] == CrcCalculator(32, 0x04c11db7, 0xffffffff, True, True, 0xffffffff, ShiftType.LEFT, ShiftType.LEFT))
+
+    data = []
+    data.append((b'123456789', b'\xb0\xd0\x0e\xc4'))
+    r = f.findCrc(data)
+    assert(len(r) == 1)
+    assert(r[0] == CrcCalculator(32, 0x04c11db7, 0xffffffff, True, True, 0xffffffff, ShiftType.LEFT, ShiftType.RIGHT, extra="result_is_le"))
